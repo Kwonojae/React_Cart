eeeaa8436e5d33f2e7847297799d3c0058d899bf
Ojea <rnjsdhwo1325@gmail.com>
2022-05-06 18:41:07 +0900

    react_cart

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import Amazon from "./components/Amazon";
+import Cart from "./components/Cart";
+import Navbar from "./components/Navbar";
 
 function App() {
+  const [show, setShow] = useState(true);
+  const [cart, setCart] = useState([]);
+
+  const handleClick = (item) => {
+    //indexOf() 메서드는 배열에서 지정된 요소를 찾을 수 있는 첫 번째
+    //인덱스를 반환하고 존재하지 않으면 -1을 반환합니다.
+    if (cart.indexOf(item) !== -1) return;
+    setCart([...cart, item]);
+    console.log(item);
+  };
+
+  const handleChange = (item, d) => {
+    const ind = cart.indexOf(item);
+    //ndexOf() 메서드는 배열에서 지정된 요소를 찾을 수 있는 첫 번째 인덱스를 반환하고 존재하지 않으면 -1을 반환합니다.
+    const arr = cart;
+    arr[ind].amount += d;
+    if (arr[ind].amount === 0) arr[ind].amount = 1;
+    setCart([...arr]);
+  };
+
+  // useEffect(() => {
+  //   console.log("cart change");
+  // }, [cart]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar setShow={setShow} size={cart.length} />
+      {show ? (
+        <Amazon handleClick={handleClick} />
+      ) : (
+        <Cart cart={cart} setCart={setCart} handleChange={handleChange} />
+      )}
     </div>
   );
 }
